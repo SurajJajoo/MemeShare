47dc04ba9188d0b412c103f04a8383d039b76de3
SurajJajoo <64190869+SurajJajoo@users.noreply.github.com>
2021-07-12 22:38:12 +0530

    MemeShare

---
+++ app/src/main/java/com/example/memeshare/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.memeshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadMeme();
+    }
+
+
+
+}
